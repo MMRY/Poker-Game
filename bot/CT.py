cdc2aec0ae35376026efb68e352b4e7d3b81ef05
elfinhuangcat <elfinhuangcat@gmail.com>
2014-04-06 16:00:08 -0400

    buggy potOddsBot + new CT.py

--- bot/CT.py
+++ bot/CT.py
@@ -23,6 +23,44 @@ def card_translate(rank, suit):
               (1 << (16+rank)))
     return prime
 
+"""translate_from_mp_to_string(card):
+   Translate a card in the MachinePoker form to the rank + suit form."""
+"""eg. 'Qd' -> (10,'d')
+   The return value is a tuple, (rank, suit)"""
+def translate_from_mp_to_string(card):
+    rank = card[0]
+    suit = card[1]
+    if rank == '2':
+        return (0,suit)
+    elif rank == '3':
+        return (1,suit)
+    elif rank == '4':
+        return (2,suit)
+    elif rank == '5':
+        return (3,suit)
+    elif rank == '6':
+        return (4,suit)
+    elif rank == '7':
+        return (5,suit)
+    elif rank == '8':
+        return (6,suit)
+    elif rank == '9':
+        return (7,suit)
+    elif rank == 'T':
+        return (8,suit)
+    elif rank == 'J':
+        return (9,suit)
+    elif rank == 'Q':
+        return (10,suit)
+    elif rank == 'K':
+        return (11,suit)
+    elif rank == 'A':
+        return (12,suit)
+    else:
+        raise ValueError('translate from mp to string:'
+                         + 'The given card is incorret!')
+    
+
 if __name__ == '__main__':
     print(card_translate(0,'s')) #2s
     print(card_translate(9,'h')) #Jh
--- bot/potOddsBot.py
+++ bot/potOddsBot.py
@@ -4,23 +4,56 @@
    to the pot odds."""
 import sys
 import json
+import monte_carlo
+import brute_force
+import CT
 
-"""
 if (len(sys.argv) < 2):
-	# exit with an error
-	sys.exit(1)
+    # exit with an error
+    debugF = open('debugPotOddsBot.txt','a')
+    debugF.write(sys.argv + '\n')
+    debugF.close()
+    sys.exit(1)
 else:
-	game_data_location = sys.argv[1]
-	game_string = open(game_data_location, 'r')
-	game_info = game_string.read()
-	game = json.loads(game_info)
-	betting = game["betting"]
-	print betting["raise"]
-	sys.exit(0)
-"""
+    game_data_location = sys.argv[1]
+    game_string = open(game_data_location, 'r')
+    game_info = game_string.read()
+    game = json.loads(game_info)
 
-
-""" Test without interacting with the MachinePoker game:"""
-""" 1. Test preflop - Monte Carlo method:"""
-
-""" 2. Test flop - brute-force method:"""
+    """1. Get the current amount in pot"""
+    pot = 0
+    for player in game["players"]:
+        pot = pot + player["wagered"]
+    
+    """2. Check the game state:"""
+    gameState = game["state"]
+    winOdds = 0
+    hole = [game["self"]["cards"][0],game["self"]["cards"][1]]
+    hole[0] = CT.translate_from_mp_to_string(hole[0])
+    hole[1] = CT.translate_from_mp_to_string(hole[1])
+    community = []
+    if len(game["community"]) > 0:
+        community = game["community"]
+        lenCom = len(community)
+        for i in range(0,lenCom):
+            community[i] = CT.translate_from_mp_to_string(community[i])
+    
+    if (gameState == "pre-flop" or
+        gameState == "flop" or
+        gameState == "turn" or
+        gameState == "river"):
+        if gameState != "river":
+            winOdds = monte_carlo.cal_win_odds_mc(hole, community)
+        else:
+            winOdds = brute_force.cal_win_odds_bf(hole, community)
+        bet = winOdds * pot / (1-winOdds)
+        if bet < game["betting"]["call"]:
+            """The largest amout you can bet < what you need to call.
+               Can only fold."""
+            print(0)
+        else:
+            print(bet)
+    else:
+        """complete"""
+        print(0)
+    sys.exit(0)
