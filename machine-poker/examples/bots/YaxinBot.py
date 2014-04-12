d9ccf20a0c437a2260434bbb64aa2e6d90ace346
elfinhuangcat <elfinhuangcat@gmail.com>
2014-04-12 17:38:46 -0400

    A new monte-carlo function is added. You can call this function cal_win_odds_mc_oppoHole(hole, community, oppoHole) when you know one or two of this opponent's hole cards. It will return your winning chance.

--- machine-poker/examples/bots/YaxinBot.py
+++ machine-poker/examples/bots/YaxinBot.py
@@ -2,23 +2,50 @@
 import sys
 import json
 
+"""YaxinBot:
+   YaxinBot will go all in if its winning chance >= 70%.
+   It will call if its winning chance >= 50% but less than 70%.
+   It will fold when the winning chance <50%.
+   """
+
 if (len(sys.argv) < 2):
-	# exit with an error
-	sys.exit(1)
+    # exit with an error
+    sys.exit(1)
 else:
-	game_data_location = sys.argv[1]
-	game_string = open(game_data_location, 'r')
-	game_info = game_string.read()
-	game = json.loads(game_info)
+    game_data_location = sys.argv[1]
+    game_string = open(game_data_location, 'r')
+    game_info = game_string.read()
+    game = json.loads(game_info)
+
+    """1. Check the game state:"""
+    gameState = game["state"]
+    winOdds = 0
+    """2. Get the cards:"""
+    hole = game["self"]["cards"]
+    community = []
+    if len(game["community"]) > 0:
+        community = game["community"]
+
+    if (gameState == "pre-flop" or
+        gameState == "flop" or
+        gameState == "turn" or
+        gameState == "river"):
+        if gameState != "river":
+            winOdds = float(monte_carlo.cal_win_odds_mc(hole, community))
+        else:
+            winOdds = float(brute_force.cal_win_odds_bf(hole, community))
+        	
 	betting = game["betting"]
-	can_raise = betting["canRaise"]
-	# TODO get win chance
-	win_chance = .75
-	if win_chance >= .70 and can_raise:
-		# go all in
-		mine = game["self"]
-		print mine["chips"]
-	elif win_chance >= .50:
-		print betting["call"]
+	canRaise = betting["canRaise"]
+	if winOdds >= .70 and canRaise:
+            # go all in
+            mine = game["self"]
+            print(mine["chips"])
+	elif winOdds >= .50:
+            print(betting["call"])
 	else: # fold
-		print 0
+            print(0)
+    else:
+        #game completes.
+        print(0)
+    sys.exit(0)
